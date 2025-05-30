9c209c7c250011bded3b2011798b54e4207b2acc
yifaaan <0219lyf@gmail.com>
2025-05-30 16:23:25 +0800

    auto grad

--- engine.py
+++ engine.py
@@ -37,102 +37,96 @@ class Value:
         self.data = data
         self.grad = 0
         self._prev = set(_children)
+        # calculate the gradient of self
+        self._backward = lambda: None
         self._op = _op
         self.label = label
     def __repr__(self):
         return f"Value(data={self.data})"
 
     def __add__(self, other):
-        return Value(self.data + other.data, (self, other), '+', )
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+', )
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
+        return out
+    
+    def __radd__(self, other):
+        return self + other
     
     def __mul__(self, other):
-        return Value(self.data * other.data, (self, other), '*')
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+        return out
+    
+    def __rmul__(self, other):
+        return self * other
     
     def __sub__(self, other):
-        return Value(self.data - other.data, (self, other), '-')
+        return self + (-other)
     
     def __truediv__(self, other):
-        return Value(self.data / other.data, (self, other), '/')
+        return self * other**-1
     
+    def __neg__(self):
+        return self * -1
+
     def __pow__(self, other):
-        return Value(self.data ** other.data, (self, other), '**')
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data ** other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += other * (self.data ** (other - 1)) * out.grad
+        out._backward = _backward
+        return out
     
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self,), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+        return out
+
     def tanh(self):
         x = self.data
         tan_h = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
-        return Value(tan_h,(self,),'tanh')
+        out = Value(tan_h,(self,),'tanh')
+
+        def _backward():
+            self.grad += (1 - tan_h**2) * out.grad
+        out._backward = _backward
+        return out
+    
+    def backward(self):
+        
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad = 1.0
+        for n in reversed(topo):
+            n._backward()
 
 def f(x):
     return 3*x**2 - 4*x + 5
 
-def lol():
-    h = 0.0001
-    a = Value(2.0, label='a')
-    b = Value(-3.0, label='b')
-    c = Value(10.0, label='c')
-    e = a * b; e.label = 'e';
-    d = e + c; d.label = 'd';
-    f = Value(-2.0, label='f')
-    L = d * f; L.label = 'L'
-    L1 = L
 
-    a = Value(2.0, label='a')
-    b = Value(-3.0, label='b')
-    b.data += h
-    c = Value(10.0, label='c')
-    e = a * b; e.label = 'e';
-    d = e + c; d.label = 'd';
-    f = Value(-2.0, label='f')
-    L = d * f; L.label = 'L'
-    L2 = L
-    print((L2 - L1)/Value(h))
-    return L1
 if __name__ == "__main__":
-    # xs = np.arange(-5, 5, 0.25)
-    # ys = f(xs)
-    # plt.plot(xs, ys)
-    # # 把画布保存到内存缓冲区
-    # buf = io.BytesIO()
-    # plt.savefig(buf, format='png')
-    # plt.close()             # 关闭 figure，节省内存
-    # buf.seek(0)
-
-    # # 通过 subprocess 把 PNG 数据发给 kitty icat
-    # subprocess.run(
-    #     ['kitty', '+kitten', 'icat'],
-    #     input=buf.read()
-    # )
-    lol()
-
-    a = Value(2.0, label='a')
-    b = Value(-3.0, label='b')
-    c = Value(10.0, label='c')
-    e = a * b; e.label = 'e';
-    d = e + c; d.label = 'd';
-    f = Value(-2.0, label='f')
-    L = d * f; L.label = 'L'
-
-    L.grad = 1
-    d.grad = -2.0
-    f.grad = 4.0
-    c.grad = d.grad * 1.0
-    e.grad = d.grad * 1.0
-    a.grad = e.grad * b.data
-    b.grad = e.grad * a.data
-
-    dot = draw_dot(L)
-    dot.render('graph', format='svg')
-
-    a.data += 0.01 * a.grad
-    b.data += 0.01 * b.grad
-    c.data += 0.01 * c.grad
-    f.data += 0.01 * f.grad
-    e = a * b;
-    d = e + c;
-    L = d * f
-    print(L)
-
-    
     # nural netwrok
     x1 = Value(2.0, label='x1')
     x2 = Value(0.0, label='x2')
@@ -146,17 +140,22 @@ if __name__ == "__main__":
     n = x1w1x2w2 + b; n.label = 'n'
     o = n.tanh()
     o.label = 'o'
-    o.grad = 1.0
-    # do/dn = (1 - o^2)
-    n.grad = o.grad * (1 - o.data**2)
-    b.grad = n.grad * 1.0
-    x1w1x2w2.grad = n.grad * 1.0
-    x1w1.grad = x1w1x2w2.grad * 1.0
-    x2w2.grad = x1w1x2w2.grad * 1.0
-    x1.grad = x1w1.grad * w1.data
-    x2.grad = x2w2.grad * w2.data
-    w1.grad = x1w1.grad * x1.data
-    w2.grad = x2w2.grad * x2.data
+
+
+    x1 = Value(2.0, label='x1')
+    x2 = Value(0.0, label='x2')
+    w1 = Value(-3.0, label='w1')
+    w2 = Value(1.0, label='w2')
+    b = Value(8, label='b')
+
+    x1w1 = x1 * w1; x1w1.label = 'x1*w1'
+    x2w2 = x2 * w2; x2w2.label = 'x2*w2'
+    x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1*w1 + x2*w2'
+    n = x1w1x2w2 + b; n.label = 'n'
+    e = (2 * n).exp()
+    o =(e-1)/(e+1)
+    o.label = 'o'
     
+    o.backward()
     dot = draw_dot(o)
     dot.render('graph', format='svg')
